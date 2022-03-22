ef822f4da1ebb9812f247f23b3770dc2f364f1ab
srikrishnalee <srikrishna@leewayhertz.com>
2022-03-22 12:42:03 +0530

    pushing the react switch and route files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route , Switch } from "react-router-dom"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import NotFound from "./components/NotFound";
+
+const App = () =>(
+   <BrowserRouter>
+     <Header />
+     <Switch>
+       <Route exact path="/" component={Home} />
+       <Route exact path="/about" component={About} />
+       <Route exact path="/contact" component={Contact} />
+       <Route component={NotFound} />
+     </Switch>
+   </BrowserRouter>
+   
+)
 
 export default App;
